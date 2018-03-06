f0edf798da5794525a150994dc13fd2b4620c1b9
NielsCharlier <niels@scitus.be>
2018-03-06 12:30:37 +0100

    Change run message to binary

--- src/community/taskmanager/src/main/java/org/geoserver/taskmanager/data/impl/RunImpl.java
+++ src/community/taskmanager/src/main/java/org/geoserver/taskmanager/data/impl/RunImpl.java
@@ -47,9 +47,9 @@ public class RunImpl extends BaseImpl implements Run {
     @Enumerated
     private Status status = Status.RUNNING;
     
-    @Column(length = Integer.MAX_VALUE)
+    @Column
     @Lob
-    private String message;
+    private byte[] message;
 
     @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "batchRun")
@@ -106,12 +106,12 @@ public class RunImpl extends BaseImpl implements Run {
 
     @Override
     public String getMessage() {
-        return message;
+        return message == null ? null : new String(message);
     }
 
     @Override
     public void setMessage(String message) {
-        this.message = message;
+        this.message = message == null ? null : message.getBytes();
     }
 
     @Override
