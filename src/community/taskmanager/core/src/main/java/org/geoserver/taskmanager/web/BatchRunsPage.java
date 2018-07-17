ff594cf06fce08d176292f72693abd5b95ad2d59
NielsCharlier <niels@scitus.be>
2018-07-17 16:31:03 +0200

    sortering batch runs

--- src/community/taskmanager/core/src/main/java/org/geoserver/taskmanager/web/BatchRunsPage.java
+++ src/community/taskmanager/core/src/main/java/org/geoserver/taskmanager/web/BatchRunsPage.java
@@ -10,6 +10,7 @@ import org.apache.wicket.Page;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.IModel;
@@ -72,6 +73,7 @@ public class BatchRunsPage extends GeoServerSecuredPage {
         add(new Form<>("form").add(runsPanel = runPanel()));
         runsPanel.setOutputMarkupId(true);
         runsPanel.setSelectable(false);
+        runsPanel.getDataProvider().setSort(BatchRunsModel.START.getName(), SortOrder.DESCENDING);
 
         add(
                 new AjaxLink<Object>("close") {
